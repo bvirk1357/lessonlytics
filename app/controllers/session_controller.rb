12a17dfa99b89be3810818c598baff150adf0c92
Bikram Virk & Marshall Hattersley <pair+bvirk1357.mwhatters@devbootcamp.com>
2015-07-28 12:15:47 -0700

    Used the same Session#Signup route/action to work for both Teachers and Students

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,18 @@
 class SessionController < ApplicationController
+
+  def signup
+
+    if params[:usertype] == 'student'
+      render 'student_signup'
+    elsif params[:usertype] == 'teacher'
+      render 'teacher_signup'
+    else
+      redirect_to root_path
+    end
+
+  end
+
+  def login
+  end
+
 end
