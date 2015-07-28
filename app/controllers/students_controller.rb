cc8a276933e08b686eb2baa47c1acb0bcd287bcf
Bikram Virk & Marshall Hattersley <pair+bvirk1357.mwhatters@devbootcamp.com>
2015-07-28 12:29:41 -0700

    Added Teacher and Student model and migration

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,13 @@
 class StudentsController < ApplicationController
+
+  def create
+    puts "Hi Student: params: #{student_params}"
+  end
+
+
+private
+  def student_params
+    params.require(:student).permit(:firstname, :lastname, :username, :password, :email)
+  end
+
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,7 @@
 class TeachersController < ApplicationController
+
+  def create
+    puts "Hi Teacher: params: #{params}"
+  end
+
 end
